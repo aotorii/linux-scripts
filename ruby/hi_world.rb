d4e5317b099f22d709265304e07cb99c7ef2250b
Josue <josuezhao@outlook.com>
2026-07-20 09:01:08 +0800

    setup stuff

--- ruby/hi_world.rb
+++ ruby/hi_world.rb
@@ -1,49 +1,49 @@
 #!/usr/bin/env ruby
 
 class WorldGreeter
-    attr_accessor :names
+  attr_accessor :names
 
-    def initialize(name="World")
-        @names=names
-    end
+  def initialize(names = 'World')
+    @names = names
+  end
 
-    def say_hi
-        if @names.nil?
-            puts "..."
-        elsif @names.respond_to?("each")
-            @names.each do |name|
-                puts "Hello #{name}!"
-            end
-        else
-            puts "Hello #{@names}!"
-        end
+  def say_hi
+    if @names.nil?
+      puts '...'
+    elsif @names.respond_to?('each')
+      @names.each do |name|
+        puts "Hello #{name}!"
+      end
+    else
+      puts "Hello #{@names}!"
     end
+  end
 
-    def say_bye
-        if @names.nil?
-            puts "..."
-        elsif @names.respond_to?("join")
-            puts "Goodbye #{@names.join(", ")}. Come back soon!"
-        else
-            puts "Goodbye #{@names}. Come back soon!"
-        end
+  def say_bye
+    if @names.nil?
+      puts '...'
+    elsif @names.respond_to?('join')
+      puts "Goodbye #{@names.join(', ')}. Come back soon!"
+    else
+      puts "Goodbye #{@names}. Come back soon!"
     end
+  end
 end
 
-if __FILE__ == $0
-    wg=WorldGreeter.new
-    wg.say_hi
-    wg.say_bye
+if __FILE__ == $PROGRAM_NAME
+  wg = WorldGreeter.new
+  wg.say_hi
+  wg.say_bye
 
-    wg.names="Epstein"
-    wg.say_hi
-    wg.say_bye
+  wg.names = 'Epstein'
+  wg.say_hi
+  wg.say_bye
 
-    wg.names=["Trump","Diddy","Netanyahu","Epstein"]
-    wg.say_hi
-    wg.say_bye
+  wg.names = %w[Trump Diddy Netanyahu Epstein]
+  wg.say_hi
+  wg.say_bye
 
-    wg.names=nil
-    wg.say_hi
-    wg.say_bye
+  wg.names = nil
+  wg.say_hi
+  wg.say_bye
 end
---
+++ ruby/lasagna.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+class Lasagna
+  EXPECTED_MINUTES_IN_OVEN = 40
+  TIME_PER_LAYER = 2
+
+  def remaining_minutes_in_oven(actual_minutes_in_oven)
+    EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
+  end
+
+  def preparation_time_in_minutes(layers)
+    layers * TIME_PER_LAYER
+  end
+
+  def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
+    number_of_layers * TIME_PER_LAYER + actual_minutes_in_oven
+  end
+end
